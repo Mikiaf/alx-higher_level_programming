92f660a80adff8b2d72fc79c8b3ef00245254238
Mikiaf <110899412+Mikiaf@users.noreply.github.com>
2022-12-06 02:57:08 -0700

    Update 8-concat_edges.py

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
-#!/usr/bin/bash
+#!/usr/bin/python
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = str[39:66] + str[106:112] + str[:6]
+str = object-oriented programming with Python
 print(str)
