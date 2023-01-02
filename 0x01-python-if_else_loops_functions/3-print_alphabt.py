dbee7d034d038dfa0fb26cc986c33bf9f820332f
root <mikiyasafework12@gmail.com>
2023-01-02 15:16:24 +0000

    task

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 for x in range(ord('a'), ord('z') + 1):
-    if x == ord('e') or x == ord('q'):
+    if x == ord('e') or ord == ord('q'):
         continue
-    print('{:c}'.format(x), end="")
+    print('{:c}'.format(x), end='')
+
