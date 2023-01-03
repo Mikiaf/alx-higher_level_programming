c6e9b3f49d690ed654e44ecc1c7f12c38471793a
Mikiaf <mikiyasafework12@gmail.com>
2023-01-02 23:13:39 -0700

    task

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for x in range(0, 100):
-    if x > 9:
-        print('{}'.format(x), end=', ')
+    if x == 99:
+        print('{}'.format(x))
     else:
-        print('0{}'.format(x),end=', ')
+        print('{:02}'.format(x), end=', ')
