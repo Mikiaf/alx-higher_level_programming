9d9292e625fcbfc90895f30957f2f8feadd71b3c
Mikiaf <mikiyasafework12@gmail.com>
2023-01-05 00:17:20 -0700

    task

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,6 +4,6 @@ def uppercase(str):
     for x in str:
         if 122 >= ord(x) >= 97:
             x = ord(x) - 32
-            print("{:c}".format(x), end='')
+            print("{:c}".format(x), end=' ')
         else:
             print("{}".format(x), end='')
