3fd8e40b97e290b8b575e83b7d1334dae54fc797
Mikiaf <mikiyasafework12@gmail.com>
2023-01-15 12:37:23 -0700

    task

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    i = 1
     arg = len(sys.argv) - 1
     if arg == 0:
         print("0 arguments.")
@@ -10,5 +9,4 @@ if __name__ == "__main__":
     else:
         print("{} arguments:".format(arg))
     for x in range(1, arg + 1):
-        print("{}: {}".format(i, sys.argv[x]))
-        i += 1
+        print("{}: {}".format(x, sys.argv[x]))
