8fdd67e06b4aa7940ca2d77de83199dde545112c
Mikiaf <mikiyasafework12@gmail.com>
2023-01-22 14:22:38 -0700

    task

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,7 +2,7 @@
 def replace_in_list(my_list, idx, element):
     if idx < 0:
         return my_list
-    if len(my_list) < idx:
+    if len(my_list) <= idx:
         return my_list
     else:
         my_list[idx] = element
