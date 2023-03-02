85a5749726dc3b679d77d9b70345d5d510cd2201
Mikiaf <mikiyasafework12@gmail.com>
2023-03-02 09:37:37 -0700

    task

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
-class list:
-    def __init__(self):
-        self.list = []
-    def append(self,value):
-        self.list.append(value)
-    mylist = self.list
 class MyList(list):
     def print_sorted(self):
-        print(list.mylist)
+        print(sorted(self))
