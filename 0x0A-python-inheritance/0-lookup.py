c5a763e03ab7b9fe76151d5953c0ffaec0ad6277
Mikiaf <mikiyasafework12@gmail.com>
2023-03-03 23:44:11 -0700

    task

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
+"""
+This python file returns 
+list of available attributes and methods of an object
+"""
 def lookup(obj):
+    """
+    return obj methods and attributes
+    """
     return dir(obj)
