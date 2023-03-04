2895b9f56542c21c3d65a41e4f81c4f5605ec18a
Mikiaf <mikiyasafework12@gmail.com>
2023-03-04 00:25:30 -0700

    task

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,5 +3,8 @@
     define class called square with Private instance attribute: size
 """
 class Square:
+    """
+    Represents a square
+    """
     def __init__(self, size):
         self.__size = size
