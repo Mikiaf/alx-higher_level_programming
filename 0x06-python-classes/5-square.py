96d183fcb447cd15eff9913118b405bc73e3c7a1
Mikiaf <mikiyasafework12@gmail.com>
2023-03-04 03:13:38 -0700

    task

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
+"""defines Square"""
+
+
 class Square:
+    """repersents square"""
+
     def __init__(self, size=0):
+        """
+        agr:size represent size
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
@@ -16,6 +24,9 @@ class Square:
 
     @size.setter
     def size(self, value):
+        """
+        arg:value represents size
+        """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
