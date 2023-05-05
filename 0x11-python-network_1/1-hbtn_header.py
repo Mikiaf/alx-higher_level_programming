721102f2c00c4ce2a3122f7e82e08cec8e00844a
Mikiaf <mikiyasafework12@gmail.com>
2023-05-05 01:01:53 -0700

    task

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,11 +3,12 @@ import urllib.request
 import sys
 
 
-"""displays the value of the X-Request-Id 
+"""displays the value of the X-Request-Id
 variable found in the header of the response.
 """
-try:
-    with urllib.request.urlopen(sys.argv[1]) as header_requist:
-        print(header_requist.headers['X-Request-Id'])
-except IndexError:
-    pass
+if __name__ == "__main__":
+    try:
+        with urllib.request.urlopen(sys.argv[1]) as header_requist:
+            print(header_requist.headers['X-Request-Id'])
+    except IndexError:
+        pass
--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -3,15 +3,16 @@ import urllib.request
 import sys
 
 """
-sends a POST request to the passed 
+sends a POST request to the passed
     URL with the email as a parameter
 """
 
-url = sys.argv[1]
-value = {'gmail': sys.argv[2]}
-data = urllib.parse.urlencode(value).encode('ascii')
+if __name__ == "__main__":
+    url = sys.argv[1]
+    value = {'gmail': sys.argv[2]}
+    data = urllib.parse.urlencode(value).encode('ascii')
 
-request = urllib.request.Request(url, data)
+    request = urllib.request.Request(url, data)
 
-with urllib.request.urlopen(request) as respond:
-    print(response.read().decode('utf-8'))
+    with urllib.request.urlopen(request) as respond:
+        print(response.read().decode('utf-8'))
